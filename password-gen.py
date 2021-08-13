bcbd947dba912c519f50050c261f7b4dbbbdb26d
Tom <55777857+dripper1234567@users.noreply.github.com>
2021-08-13 11:05:01 +0800

    Adjusted limmits

--- password-gen.py
+++ password-gen.py
@@ -4,7 +4,7 @@ import random
 def generate(length=8):
     passw = ""
     for char in range(0, length+1):
-        passw += chr(random.randint(65, 90))
+        passw += chr(random.randint(65, 122))
     return passw
 
 
